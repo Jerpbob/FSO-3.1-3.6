8f640bffde7c2fefdbcd5ebb7b6c366f954e0015
Jeru Balares <jerupaulbalares@gmail.com>
2023-01-24 15:16:44 -0800

    Extra stuff that I don't know even what they are

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+app.use(cors())
 
 let persons = [
     {
@@ -79,7 +81,7 @@ app.post('/api/persons', (request, response) => {
     response.json(persons)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
 })
